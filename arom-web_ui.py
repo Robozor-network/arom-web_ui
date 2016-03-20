013118ec5603c59725ec63a67a5d9f4e66d62480
ODROID <roman-dvorak@email.cz>
2016-03-20 21:29:35 +0100

    Add skyMap, move mount to AltAz, simple gui, restructure layout, ...

--- arom-web_ui.py
+++ arom-web_ui.py
@@ -13,13 +13,66 @@ from tornado import httpserver
 from tornado import options
 from tornado import web
 import json
+import ephem
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import Angle
+from astropy.coordinates import EarthLocation
+from astropy.coordinates import AltAz
+from astropy.time import Time
+import rosapi
+
+#print rosapi.get_nodes()
+
+observatory = EarthLocation(lat=49*u.deg, lon=14*u.deg, height=300*u.m)
+
+leftmenu = [["observatory", "observatory"],["Vec 1", "#"], ["Vec 2", "#"], ["Vec 3", "#"], ["Mount", "mount"]]
+leftmenu = [["observatory", "observatory"],["Vec 1", "#"], ["Vec 2", "#"], ["Vec 3", "#"], ["Mount", "mount"]]
 
 
 class Overview(web.RequestHandler):
     @tornado.web.asynchronous
     def get(self, addres=None):
         print "web", addres
-        self.render("www/layout/dash/publicOverview.html", title = "MyTitle")
+        self.render("www/layout/dash/publicOverview.html", title = "MyTitle", leftmenu = leftmenu, actual = '#')
+
+class Mount(web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, addres=None):
+        print "web", addres
+        self.render("www/layout/dash/mount.html", title = "MyTitle | mount", leftmenu = leftmenu, actual = 'mount')
+
+class Observatory(web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, addres=None):
+        print "web", addres
+        self.render("www/layout/dash/observatory.html", title = "MyTitle | observatory", leftmenu = leftmenu, actual = 'observatory')
+
+class processing(web.RequestHandler):
+    #@tornado.web.asynchronous
+    def get(self, arg = None):
+        print "processing", arg
+        coord = self.get_argument("coord", False)
+        if coord:
+            param = eval(coord)
+            print "param", param
+            if param['typ'] == 'AltAz2RaDec':
+                out = SkyCoord(alt=param['alt'], az=param['az'], unit='deg', obstime = Time.now(), frame = 'altaz', location = observatory)
+                sout = {
+                    "ra": out.icrs.ra.degree,
+                    "dec": out.icrs.dec.degree,
+                    #"alt": param['alt'],
+                    #"az": param['az'],
+                    }
+                print sout
+                self.write(escape.json_encode(sout))
+
+                
+            elif param['typ'] == 'RaDec2AltAz':
+                # = loc.transform_to(AltAz(obstime = Time.now(), location=self.observatory))
+                #F_az = (np.abs(self.horizont[:,0] - altaz.az.degree)).argmin()
+                #local_horizont = self.horizont[F_az]
+                pass
 
 
 
@@ -27,8 +80,11 @@ class Overview(web.RequestHandler):
 app = web.Application([
         (r'/', Overview),
         (r'/index', Overview),
-       # (r'/ws', SocketHandler),
-       # (r'/clients', ClientsHandler),
+        (r'/mount', Mount),
+        (r'/observatory', Observatory),
+        (r'/obs', Observatory),
+        (r'/processing', processing),
+        (r'/processing(.*)', processing),
        # (r'/multibolid(.*)', MultiBolid),
        # (r'/multibolid', MultiBolid),
        # (r'/realtime(.*)', RTbolidozor),
@@ -39,6 +95,8 @@ app = web.Application([
         (r"/(.*\.jpg)", tornado.web.StaticFileHandler,{"path": './www/media/' }),
         (r"/(.*\.css)", tornado.web.StaticFileHandler,{"path": './www/css/' }),
         (r"/(.*\.wav)", tornado.web.StaticFileHandler,{"path": './www/wav/' }),
+        (r"/(.*\.json)", tornado.web.StaticFileHandler,{"path": './www/json/' }),
+        (r"/(.*\.js)", tornado.web.StaticFileHandler,{"path": './www/js/' }),
        #(r"/static/(.*)", web.StaticFileHandler, {"path": "/var/www"}),
         (r"/(.*)", Overview),
     ],
